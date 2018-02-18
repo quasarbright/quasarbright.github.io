b10e5672eb68427023e1c8d1b76b11ec0c6ce627
quasarbright <mthundershock@gmail.com>
2018-02-18 18:21:26 -0500

    delete bad stuff and rename some stuff

--- p5js/spanning tree/sketch.js
+++ p5js/spanning tree/sketch.js
@@ -1,39 +1,63 @@
-const vertices = [] //"unplaced" vertices
-const indices = [] //vertices that are in the final graph
-const edges = [] //[[v1,v2],...] list of vector pairs
-const numberOfVertices = 8
-let startVertex;
+let vertices = []
 
 function setup() {
-    createCanvas(400, 400);
-    for (let i = 0; i < numberOfVertices; i++) {
-        v = {
-            pos: createVector(random(width), random(height)),
-            value: Infinity
-        }
-        if (i === 0) v.value = 0
-        vertices.push(v)
+    createCanvas(800, 600);
+    for (let i = 0; i < 100; i++) {
+        vertices.push(createVector(random(width), random(height)))
     }
 }
 
-function iterate() {
-    if (vertices.length === 0) return null //exit the function
-    let minIndex = 0; //index of the minimum cost startVertex
-    for (let i = 0; i < vertices.length; i++) {
-        if (vertices[i].value < vertices[minIndex].value) minIndex = i
-    }
-
+function mousePressed() {
+    vertices.push(createVector(mouseX, mouseY))
+    loop()
 }
 
-function cost(v1, v2) {
-    return v1.copy().sub(v2).mag()
+function clearVertices(){
+    vertices = []
+    loop()
 }
 
 function draw() {
     background(51);
-    for (let v of vertices) {
-        stroke(255, 0, 0)
-        strokeWeight(10)
-        point(v.pos.x, v.pos.y)
+
+    let reached = []
+    let unreached = vertices.map((e) => e) //copy
+
+    reached.push(unreached[0])
+    unreached.splice(0, 1)
+
+    while (unreached.length > 0) {
+        let record = Infinity
+        let rind;
+        let uind;
+
+        for (var i = 0; i < reached.length; i++) {
+            for (var j = 0; j < unreached.length; j++) {
+                let v1 = reached[i]
+                let v2 = unreached[j]
+                let d = dist(v1.x, v1.y, v2.x, v2.y)
+
+                if (d < record) {
+                    record = d
+                    rind = i
+                    uind = j
+                }
+            }
+        }
+        stroke(255)
+        strokeWeight(2)
+        line(reached[rind].x,reached[rind].y,unreached[uind].x,unreached[uind].y)
+        reached.push(unreached[uind])
+        unreached.splice(uind,1)
     }
+
+    for (v of vertices) {
+        stroke(255)
+        strokeWeight(10)
+        point(v.x, v.y)
+    }
+    stroke(255,0,0)
+    strokeWeight(10)
+    point(vertices[0].x, vertices[0].y)
+    noLoop()
 }
