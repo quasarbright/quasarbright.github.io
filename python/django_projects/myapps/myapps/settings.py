97cba277ff42466299e4739646bc16160f600ffc
quasarbright <mthundershock@gmail.com>
2018-03-24 13:15:51 -0400

    create myapps index page

--- python/django_projects/myapps/myapps/settings.py
+++ python/django_projects/myapps/myapps/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myapps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['finances/templates','myapps/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- python/django_projects/myapps/myapps/urls.py
+++ python/django_projects/myapps/myapps/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('finances/',include('finances.urls')),
-    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('finances/',include('finances.urls'),name='finances'),
+    path('admin/', admin.site.urls,name='admin'),
 ]
