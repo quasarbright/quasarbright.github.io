ed2d41f73be6755b97941ae2b7b846e9be01488b
quasarbright <mthundershock@gmail.com>
2018-03-25 00:40:21 -0400

    tidying

--- python/django_projects/myapps/finances/views.py
+++ python/django_projects/myapps/finances/views.py
@@ -1,30 +1,40 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from .forms import TransactionForm
-from .models import Transaction
 from django.utils import timezone
-from .reference import TYPE_CHOICES
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import TransactionForm
+from .models import Transaction
+from .reference import TYPE_CHOICES
 
 # Create your views here.
+
+
 class TransactionView(LoginRequiredMixin, generic.detail.DetailView):
     model = Transaction
     context_object_name = 'transaction'
     template_name = 'finances/viewTransaction.html'
+
+
 class TransactionsView(LoginRequiredMixin, generic.ListView):
-    model=Transaction
+    model = Transaction
     context_object_name = 'transactions'
     template_name = 'finances/view.html'
+
+
 @login_required
 def index(request):
     return render(request, 'finances/index.html')
-class TransactionFormView(LoginRequiredMixin,generic.edit.FormView):
-    template_name='finances/insert.html'
-    context_object_name='form'
+
+
+class TransactionFormView(LoginRequiredMixin, generic.edit.FormView):
+    template_name = 'finances/insert.html'
+    context_object_name = 'form'
     form_class = TransactionForm
+
+
 @login_required
 def insertResult(request):
     amount = request.POST['amount']
@@ -35,7 +45,9 @@ def insertResult(request):
             context_type = pair[1]
             break
     description = request.POST['description']
-    context = {'amount':amount,'type':context_type,'description':description}
-    transaction = Transaction.objects.create(date=timezone.now(),amount=amount,type=type,description=description)
+    context = {'amount': amount, 'type': context_type,
+               'description': description}
+    transaction = Transaction.objects.create(
+        date=timezone.now(), amount=amount, type=type, description=description)
     pk = transaction.id
-    return HttpResponseRedirect(reverse('finances:view_transaction',args=[pk]))
+    return HttpResponseRedirect(reverse('finances:view_transaction', args=[pk]))
