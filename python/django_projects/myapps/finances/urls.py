fa001623493445328113e919a20aa2e6beac5b2c
quasarbright <mthundershock@gmail.com>
2018-03-25 00:40:03 -0400

    tidying

--- python/django_projects/myapps/finances/urls.py
+++ python/django_projects/myapps/finances/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'finances'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('transactions/view/<int:pk>',views.TransactionView.as_view(),name="view_transaction"),
-    path('transactions/view',views.TransactionsView.as_view(),name="view"),
-    path('transactions/insert',views.TransactionFormView.as_view(),name='insert'),
-    path('transactions/insertResult',views.insertResult,name='insertResult'),
+    path('transactions/view/<int:pk>',
+         views.TransactionView.as_view(), name="view_transaction"),
+    path('transactions/view', views.TransactionsView.as_view(), name="view"),
+    path('transactions/insert', views.TransactionFormView.as_view(), name='insert'),
+    path('transactions/insertResult', views.insertResult, name='insertResult'),
 ]
