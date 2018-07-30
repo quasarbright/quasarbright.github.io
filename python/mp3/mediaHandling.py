071ed01f2ab368fbcf2b2a5cc586ed24c37051a1
Mike Delmonaco <mthundershock@gmail.com>
2018-07-30 15:01:02 -0400

    fixed a lot of bugs with on_eos and stuff
    edge conditions such as running out of songs, then queueing now work properly. Hacks were used, but hey, it works.

--- python/mp3/mediaHandling.py
+++ python/mp3/mediaHandling.py
@@ -1,8 +1,21 @@
+'''
+loop() executes every second until stopLoop() is called and loop() calls
+update(), which checks for song ending. If song ended, it calls on_eos(), which
+handles queueing the next song.
+tests to run:
+- queue playlist and qutoplay works
+- get to end of queue, finish song, wait, then queue another song and it should auto-play
+- next and previous should work
+- forcePlay should work
+- playing one song by itself should work (nothing else in queue)
+- one song in queue, finish, previousSong, finish, should reach end of queue
+- start loop then queue and play works
+'''
 import os
-import random
+import sys
 import threading
 import DBhandling as db
-from playlistLogic import buildPlaylist
+import playlistLogic as pl
 from pyglet.media import load, Player
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -11,15 +24,20 @@ song_dir = os.path.join(script_dir, 'songs')
 player = Player()
 songQueue = []
 songIndex = 0
-previousSource = player.source
+reachedEnd = False
 
 def play():
     player.play()
 
 def pause():
     player.pause()
+
 def seek(time):
     player.source.seek(time)
+
+def quit():
+    stop()
+    sys.exit()
 
 
 def seekRatio(x):
@@ -32,20 +50,85 @@ def getQueue():
 
 def getPlaying():
     '''returns source that is playing or False if there is none'''
-    if len(songQueue) != 0:
+    if len(songQueue) != 0 and len(songQueue) > songIndex:
         return songQueue[songIndex]
     return False
 
+def nextSong():
+    '''skips to next song and returns True, returns False and does nothing if
+    there is no next song
+    '''
+    if songIndex != len(songQueue) - 1:
+        player.next_source()
+        on_eos()
+        return True
+    return False
+
+def previousSong():
+    '''goes to previous song or the beginning of current song if there is no
+    previous song
+    '''
+    global songIndex, songQueue, reachedEnd
+    reachedEnd = False
+    if songIndex > 0:
+        songIndex -= 1
+    player.next_source()
+    realQueue(songQueue[songIndex])
+    play()
+
+def queue(song):
+    song = db.getSong(song)
+    songQueue.append(song)
+    if len(songQueue) == 1:
+        realQueue(song)
+
+def getSource(song):
+    return load(songPath(song))
+
+def realQueue(song):
+    # actually queue the song on the player
+    print('really queueing', song)
+    player.queue(getSource(song))
+
 def update():
     # check for eos
-    global player, previousSource, on_eos
-    if player.source != previousSource:
+    global player, on_eos, reachedEnd
+    if player.playing:
+        reahchedEnd = False
+    if player.source != None and abs(player.source.duration - player.time < .001):
         on_eos()
-    previousSource = player.source
+
 def on_eos():
-    print('sdfsdf')
+    global songIndex, songQueue, player, load, songPath, reachedEnd
+    if len(songQueue) > songIndex:
+        print(songQueue[songIndex], 'ended')
+    if len(songQueue) == songIndex:
+        print('reached end of queue')
+        reachedEnd = True
+    if len(songQueue) > songIndex and not reachedEnd:
+        songIndex += 1
+    if len(songQueue) > songIndex:
+        # if there is a current song
+        reachedEnd = False
+        realQueue(songQueue[songIndex])
+        player.play()
 
+def reset():
+    global player, songQueue, songIndex
+    player = Player()
+    songQueue = []
+    songIndex = 0
 
+def getNextSong():
+    if songIndex < len(songQueue) - 1:
+        return songQueue[songIndex + 1]
+
+def forcePlay(song):
+    '''plays a song right now'''
+    songQueue.insert(songIndex+1, song)
+    nextSong()
+
+# looping
 shouldLoop = True
 def loop():
     # put stuff to be executed every second here
@@ -55,9 +138,14 @@ def loop():
     if shouldLoop:
         threading.Timer(1, loop).start()
 
-def stop():
+def stopLoop():
     global shouldLoop
     shouldLoop = False
+
+def resumeLoop():
+    global shouldLoop
+    shouldLoop = True
+    loop()
 
 '''
 you don't want queue to work like it currently does.
@@ -71,22 +159,26 @@ def songPath(song):
     song = db.getSong(song)
     return os.path.join(song_dir, song.name+'.mp3')
 
-def queuePlaylist(playlist, shuffle=False):
+def queuePlaylist(playlist):
     playlist = db.getPlaylist(playlist)
-    songs = buildPlaylist(playlist)
-    if shuffle:
-        random.shuffle(songs)
+    songs = pl.buildPlaylist(playlist)
     for song in songs:
-        source = load(songPath(song))
-        player.queue(source)
+        queue(song)
+        # source = load(songPath(song))
 
+
+def initialize():
+    loop()
 
 if __name__ == '__main__':
-    loop()
-    queuePlaylist(db.getPlaylist('test'))
+    # queuePlaylist(db.getPlaylist('test'))
+    queue(pl.buildPlaylist(db.getPlaylist('test')).pop())
     play()
-    player.volume = .5
+    player.volume = .3
+    songIndex = len(songQueue) - 2
+    nextSong()
     seekRatio(.97)
+    loop()
 # play = player.play
 # pause = player.pause
 # seek = player.seek
