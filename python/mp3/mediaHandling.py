c27e26a175c8560d4ac806699b57757ba82ef000
Mike Delmonaco <mthundershock@gmail.com>
2018-07-30 09:41:48 -0400

    working on_eos

--- python/mp3/mediaHandling.py
+++ python/mp3/mediaHandling.py
@@ -1,6 +1,6 @@
 import os
 import random
-from queue import Queue
+import threading
 import DBhandling as db
 from playlistLogic import buildPlaylist
 from pyglet.media import load, Player
@@ -8,18 +8,64 @@ from pyglet.media import load, Player
 script_dir = os.path.dirname(os.path.realpath(__file__))
 song_dir = os.path.join(script_dir, 'songs')
 
-class MusicPlayer(Player):
-    def __init__(self):
-        super().__init__(self)
-        self.songQueue = Queue()
-        self.songIndex = 0
+player = Player()
+songQueue = []
+songIndex = 0
+previousSource = player.source
 
-    def eos():
-        
+def play():
+    player.play()
+
+def pause():
+    player.pause()
+def seek(time):
+    player.source.seek(time)
 
 
+def seekRatio(x):
+    '''expects x from 0 to 1'''
+    seek(x*player.source.duration)
 
-player = MusicPlayer()
+def getQueue():
+    '''returns songs that are being played or waiting to be played'''
+    return songQueue[songIndex:]
+
+def getPlaying():
+    '''returns source that is playing or False if there is none'''
+    if len(songQueue) != 0:
+        return songQueue[songIndex]
+    return False
+
+def update():
+    # check for eos
+    global player, previousSource, on_eos
+    if player.source != previousSource:
+        on_eos()
+    previousSource = player.source
+def on_eos():
+    print('sdfsdf')
+
+
+shouldLoop = True
+def loop():
+    # put stuff to be executed every second here
+    global shouldLoop
+    global update
+    update()
+    if shouldLoop:
+        threading.Timer(1, loop).start()
+
+def stop():
+    global shouldLoop
+    shouldLoop = False
+
+'''
+you don't want queue to work like it currently does.
+features you need to make:
+- view the queue
+- edit the queue
+- previous song
+'''
 
 def songPath(song):
     song = db.getSong(song)
@@ -34,7 +80,22 @@ def queuePlaylist(playlist, shuffle=False):
         source = load(songPath(song))
         player.queue(source)
 
-play = player.play
-pause = player.pause
-seek = player.seek
-next = player.next_source()
+
+if __name__ == '__main__':
+    loop()
+    queuePlaylist(db.getPlaylist('test'))
+    play()
+    player.volume = .5
+    seekRatio(.97)
+# play = player.play
+# pause = player.pause
+# seek = player.seek
+# next = player.next_source()
+
+
+# ### testing eos ###
+# queuePlaylist(db.getPlaylist('test'))
+# player.play()
+# player.seekRatio(.97)
+#
+# # e=input()
