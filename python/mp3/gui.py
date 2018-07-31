cb6d59579c39b83d7923e39c50f09267b7fa0657
Mike Delmonaco <mthundershock@gmail.com>
2018-07-31 15:07:19 -0400

    working tab view, add songs, media control

--- python/mp3/gui.py
+++ python/mp3/gui.py
@@ -1,18 +1,82 @@
+import os
+import ntpath
 from appJar import gui
-app = gui('main menu', '400x400')
-# for x in range(4):
-#     app.addLabel(x, 'd')
-app.startScrollPane('songPane', row=0, rowspan=3, colspan=3)
-for x in range(4, 100):
-    app.addLabel(str(x))
-app.stopScrollPane()
-with app.frame('left', row=3, column=0):
-    app.addLabel('1')
-with app.frame('mid', row=3, column=1):
-    app.addLabel('2')
-with app.frame('right', row=3, column=2):
-    app.addLabel('3')
+import DBhandling as db
+import mediaHandling as media
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+app = gui('main menu', '600x500', showIcon=True)
+media.initialize()
+media.queuePlaylist(db.getPlaylist('test'))# testing
+
+
 def stopFunction():
-    print('k bye')
+    # stop the infinite loop in media
+    media.stopLoop()
     return True
+app.setStopFunction(stopFunction)
+
+app.setIcon(os.path.join(script_dir, 'icon.gif'))
+
+
+def songPress(button):
+    if button == 'add song':
+        path = app.openBox('select a song to add', fileTypes=[('Audio', '*.mp3')])
+        name = ntpath.basename(path)
+        app.infoBox('test', path+'\n'+name)
+        bangericity = 0.0
+        while True:
+            bangericity = app.floatBox('bangericity input', 'enter a bangericity from 0 to 100')
+            if 0 <= bangericity and bangericity <= 100:
+                break
+            app.errorBox('invalid bangericity box', 'Error: bangericity must be between 0 and 100')
+        db.addSong(path, name, bangericity)
+        updateSongTable()
+
+with app.tabbedFrame('tabs'):
+    with app.tab('songs'):
+        songs = db.getAllSongs()
+        tableArr = [[song.name, song.bangericity] for song in songs]
+        app.addTable('song table', [['Name','Bangericity']]+tableArr, colspan=3)
+        #buttons
+        app.addButton('add song', songPress)
+    with app.tab('playlists'):
+        playlists = db.getAllPlaylists()
+        tableArr = [[playlist.name] for playlist in playlists]
+        app.addTable('playlist table', [['Name']]+tableArr, colspan=3)
+    with app.tab('tags'):
+        tags = db.getAllTags()
+        tableArr = [[tag.name] for tag in tags]
+        app.addTable('tag table', [['Name']]+tableArr, colspan=3)
+
+def updateSongTable():
+    songs = db.getAllSongs()
+    tableArr = [[song.name, song.bangericity] for song in songs]
+    app.replaceAllTableRows('song table', [['Name','Bangericity']]+tableArr)
+
+def updatePlaylistTable():
+    playlists = db.getAllPlaylists()
+    tableArr = [[playlist.name] for playlist in playlists]
+    app.replaceAllTableRows('playlist table', [['Name']]+tableArr)
+
+def updateTagTable():
+    tag = db.getAllTags()
+    tableArr = [[tag.name] for tag in tags]
+    app.replaceAllTableRows('tag table', [['Name']]+tableArr)
+
+
+def mediaPress(button):
+    if button == 'previous':
+        media.previousSong()
+    elif button == 'pause':
+        media.pause()
+    elif button == 'play':
+        media.play()
+    elif button == 'next':
+        media.nextSong()
+
+with app.frame('bottom', row=3):
+    app.addScale('slider', row=1)
+    app.addButtons(['previous', 'pause', 'play', 'next'], mediaPress)
+
 app.go()
