79143c9cf94c85305700bfe245f47f4db766d323
quasarbright <mthundershock@gmail.com>
2018-08-01 19:34:29 -0400

    initialize database

--- python/mp3/initialize.py
+++ python/mp3/initialize.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+subprocess.call(['python', 'mappings.py'])
 script_dir = os.path.dirname(os.path.realpath(__file__))
 try:
     os.mkdir(os.path.join(script_dir, 'songs'))
