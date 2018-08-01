54c196ff80453b3d2806461875a0cac1adfe98bf
Mike Delmonaco <mthundershock@gmail.com>
2018-08-01 18:57:32 -0400

    actions for songs

--- python/mp3/gui.py
+++ python/mp3/gui.py
@@ -1,5 +1,6 @@
 import os
 import ntpath
+import sys
 import threading
 import time
 from appJar import gui
@@ -22,27 +23,28 @@ def stopFunction():
 app.setStopFunction(stopFunction)
 
 
-def songPress(button):
-    if button == 'add song':
-        path = app.openBox('select a song to add', fileTypes=[('Audio', '*.mp3')])
-        if path:
-            name = ntpath.basename(path)
-            app.infoBox('test', path+'\n'+name)
-            bangericity = 0.0
-            while True:
-                bangericity = app.floatBox('bangericity input', 'enter a bangericity from 0 to 100')
-                if bangericity is None:
-                    # user cancelled
-                    break
-                if 0 <= bangericity and bangericity <= 100:
-                    break
-                app.errorBox('invalid bangericity box', 'Error: bangericity must be between 0 and 100')
-            if bangericity is not None:
-                try:
-                    db.addSong(path, name, bangericity)
-                except FileExistsError:
-                    app.errorBox('duplicate song error box', 'Error: A song with this name already exists. The song will not be added.')
-                updateSongTable()
+def addSong():
+    path = app.openBox('select a song to add', fileTypes=[('Audio', '*.mp3')])
+    if path:
+        name = ntpath.basename(path)
+        app.infoBox('test', path+'\n'+name)
+        bangericity = 0.0
+        while True:
+            bangericity = app.floatBox('bangericity input', 'enter a bangericity from 0 to 100')
+            if bangericity is None:
+                # user cancelled
+                break
+            # validate bangericity
+            if 0 <= bangericity and bangericity <= 100:
+                break
+            app.errorBox('invalid bangericity box', 'Error: bangericity must be between 0 and 100')
+        if bangericity is not None:
+            try:
+                db.addSong(path, name, bangericity)
+            except FileExistsError:
+                app.errorBox('duplicate song error box',
+                    'Error: A song with this name already exists. The song will not be added.')
+            updateSongTable()
 
 
 realSlide = True
@@ -68,6 +70,67 @@ def updateSlider():
         app.setScale('slider', ratio*100)
         realSlide = True
 
+def songAction(rowNum):
+    songOptionsWindow(app.getTableRow('song table', rowNum)[0])
+
+def temporaryWindow(title, body):
+    '''
+    produces and opens destructible subwindow.
+    - title is the title of the subwindow
+    - body is a function where the contents of the window are specified (adding
+    buttons, etc.)
+    '''
+    global isDestroying
+    # prevents stopFunction - destroySubWindow infinite recursion
+    isDestroying = False
+    def destroy():
+        global isDestroying
+        if not isDestroying:
+            isDestroying = True
+            app.destroySubWindow(title)
+        return True
+    app.startSubWindow(title, modal=True, transient=True)
+    app.setStopFunction(destroy)
+    body()
+    app.stopSubWindow()
+    app.showSubWindow(title)
+
+
+
+def songOptionsWindow(song):
+    song = db.getSong(song)
+    def body():
+        def press(button):
+            if button == 'play now':
+                media.forcePlay(song)
+            elif button == 'add to queue':
+                media.queue(song)
+            elif button == 'add tags':
+                pass
+            elif button == 'remove tags':
+                pass
+            elif button == 'change bangericity':
+                pass
+            elif button == 'remove from library':
+                db.removeSong(song)
+                updateSongTable()
+                ### left off here
+                '''
+                - it appears that songs like tear rain and
+                crossing field (those with album art) crash
+                the app when played. Possibly investigate
+                - need to implement functionality in these buttons
+                - should try to close window or disable buttons
+                after removing from library. Or maybe just error
+                box if they try to do something to the song
+                '''
+
+        app.addButtons(['play now', 'add to queue', 'add tags', 'remove tags', 'change bangericity', 'remove from library'], press)
+    title = song.name+' options'
+    temporaryWindow(title, body)
+
+
+
 def loop():
     global updateSlider
     while threading.main_thread().is_alive():
@@ -80,37 +143,35 @@ with app.tabbedFrame('tabs'):
         songs = db.getAllSongs()
         tableArr = [[song.name, song.bangericity] for song in songs]
         tableArr.sort()
-        app.addTable('song table', [['Name','Bangericity']]+tableArr, colspan=3)
-        #buttons
-        app.addButton('add song', songPress)
+        app.addTable('song table', [['Name','Bangericity']]+tableArr, colspan=3, addRow=addSong, showMenu=True, action=songAction)
     with app.tab('playlists'):
         playlists = db.getAllPlaylists()
         tableArr = [[playlist.name] for playlist in playlists]
         tableArr.sort()
-        app.addTable('playlist table', [['Name']]+tableArr, colspan=3)
+        app.addTable('playlist table', [['Name']]+tableArr, colspan=3, showMenu=True)
     with app.tab('tags'):
         tags = db.getAllTags()
         tableArr = [[tag.name] for tag in tags]
         tableArr.sort()
-        app.addTable('tag table', [['Name']]+tableArr, colspan=3)
+        app.addTable('tag table', [['Name']]+tableArr, colspan=3, showMenu=True)
 
 def updateSongTable():
     songs = db.getAllSongs()
     tableArr = [[song.name, song.bangericity] for song in songs]
     tableArr.sort()
-    app.replaceAllTableRows('song table', [['Name','Bangericity']]+tableArr)
+    app.replaceAllTableRows('song table', tableArr)
 
 def updatePlaylistTable():
     playlists = db.getAllPlaylists()
     tableArr = [[playlist.name] for playlist in playlists]
     tableArr.sort()
-    app.replaceAllTableRows('playlist table', [['Name']]+tableArr)
+    app.replaceAllTableRows('playlist table', tableArr)
 
 def updateTagTable():
     tag = db.getAllTags()
     tableArr = [[tag.name] for tag in tags]
     tableArr.sort()
-    app.replaceAllTableRows('tag table', [['Name']]+tableArr)
+    app.replaceAllTableRows('tag table', tableArr)
 
 
 def mediaPress(button):
