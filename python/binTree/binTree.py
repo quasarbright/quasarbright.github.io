25aff261ab8bf5d00c82816b00c0a11c0df648d1
quasarbright <mthundershock@gmail.com>
2018-10-27 16:38:27 -0400

    implemented to string

--- python/binTree/binTree.py
+++ python/binTree/binTree.py
@@ -9,13 +9,13 @@ class InvalidBinTreeError(Exception):
 
 class BT:
     def __init__(self, num, left=None, right=None):
-        #input validation
-        if not (isis)
+        # input validation
         if not (isinstance(left, BT) or left == None):
             raise InvalidBinTreeError("left tree is not a valid BT: {0}".format(left))
         if not (isinstance(right, BT) or right == None):
             raise InvalidBinTreeError("right tree is not a valid BT: {0}".format(right))
 
+        self.num = num
         self.left = left
         self.right = right
 
@@ -30,24 +30,32 @@ class BT:
         else:
             return self.left.flatten() + self.right.flatten()
 
+    def __Str__(self):
+        if self.left == None and self.right == None:
+            return "({0})".format(self.num)
+        elif self.left == None:
+            return "({0}, (), {1})".format(self.num, str(self.right))
+        elif self.right == None:
+            return "({0}, {1}, ())".format(self.num, str(self.left))
+        else:
+            return "({0}, {1}, {2})".format(self.num, str(self.left), str(self.right))
+
 
 class UnorderedBSTError(Exception):
     pass
 
 
 class BST(BT):
-    def __init__(self, left, right):
-        super().__init__(self, left, right)
-
+    def __init__(self, num, left=None, right=None):
+        super().__init__(num, left, right)
         # validate tree structure
-        '''if not self.isValid():
-            raise UnorderedBSTError("BST is not properly ordered")'''
+        #if not self.isValid():
+            #raise UnorderedBSTError("BST is not properly ordered")
 
     def isValid(self):
         '''is the tree a valid binary search tree?'''
         flattened = self.flatten()
-        ans = True
         for i in range(0, len(flattened) - 1):
             if flattened[i] < flattened[i + 1]:
-                ans = False
-        return ans
+                return False
+        return True
