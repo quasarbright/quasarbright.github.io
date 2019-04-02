2a3d4ddee3c6ac182d65c3a7cd81153e4edbc3d9
quasarbright <mthundershock@gmail.com>
2019-04-02 13:21:52 -0400

    almost there

--- python/stringMorph.py
+++ python/stringMorph.py
@@ -26,10 +26,16 @@ def swap(s, i, j):
     return ans
 
 
+def replace(s, i, letter):
+    return s[:i] + letter + s[i+1:]
+
+
 def generate_possible_moves(s):
     moves = []
     for i in range(len(s)):
         moves.append((remove, (s, i)))
+        for letter in alphabet:
+            moves.append((replace, (s, i, letter)))
         for j in range(i+1, len(s)):
             if i != j:
                 move = (swap, (s, i, j))
@@ -38,6 +44,36 @@ def generate_possible_moves(s):
     for i in range(len(s)+1):
         for letter in alphabet:
             moves.append((add, (s, i, letter)))
+    return moves
+
+
+def generate_good_moves(word, target):
+    s = word
+    # eliminates some repetition of equivalent sequences by getting lengths equal first,
+    # then manipulating.
+    moves = []
+    for i in range(len(s)):
+        # only remove if word is bigger
+        if len(word) > len(target):
+            moves.append((remove, (s, i)))
+        # only replace/swap if lengths equal
+        elif len(word) == len(target):
+            # only replace if stuff needs to be replaced
+            if frozenset(list(word)) != frozenset(list(target)):
+                for letter in alphabet:
+                    moves.append((replace, (s, i, letter)))
+            else:
+                # only swap if you only need to permute
+                for j in range(i+1, len(s)):
+                    if i != j:
+                        move = (swap, (s, i, j))
+                        if move not in moves:
+                            moves.append(move)
+    # only add if word is smaller
+    if len(word) < len(target):
+        for i in range(len(s)+1):
+            for letter in alphabet:
+                moves.append((add, (s, i, letter)))
     return moves
 
 
@@ -53,71 +89,42 @@ def generate_possible_next_states(s):
             next_states_with_moves.add((next_state, move))
     return next_states_with_moves
 
-"""
-def min_path(word, target):
-    '''
-    return minimum length list of moves
-    '''
-    # start string -> best path
-    # path stored as a list of moves
-    mem = {}
-    def help(a, maxlen):
-        '''
-        computes the best path from a to target
-        may return none if maxlen is too short
-        returns [] if it reaches the target
-        '''
-        if a in mem:
-            return mem[a]
-        if a == target:
-            return []
-        if maxlen == 0:
-            return None
-        nextMoves = generate_possible_moves(a)
-        bestPath = None
-        bestPathLength = float('inf')
-        for move in nextMoves:
-            f, args = move
-            nextWord = f(*args)
-            # best path from nextWord to target
-            bestRestPath = help(nextWord, maxlen-1)
-            if bestRestPath is not None:
-                path = [move] + bestRestPath
-                if len(path) < bestPathLength:
-                    bestPath = path
-                    bestPathLength = len(path)
-        mem[a] = bestPath
-        return bestPath
-    # don't let any sequence of transformations get longer than removing
-    # everything from a and adding b to it
-    return help(word, len(word) + len(target))
-"""
+
 # breadth first
 def min_path(word, target):
     if word == target:
         return []
-    # list of (word, move) pairs
-    queue = [(word, None)]
+    # list of (word, [move]) pairs
+    queue = []
+    nextMoves = generate_good_moves(word, target)
+    for move in nextMoves:
+        f, args = move
+        nextWord = f(*args)
+        if nextWord == target:
+            return [move]
+        queue.append((nextWord, [move]))
     seenWords = set([])
     last = None
-    while len(queue) > 0:
-        curr = queue.popLeft()
-
-        if curr not in seenWords:
-            seenWords.add(curr)
-            a, prevMove = curr
-            prevWord = prevMove[0]
-            nextMoves = generate_possible_moves(a)
+    while len(queue) > 0 and last is None:
+        import pdb; pdb.set_trace()
+        curr = queue.pop(0)
+        a, prevMoves = curr
+        if a not in seenWords:
+            seenWords.add(a)
+            prevWord = prevMoves[-1][0]
+            nextMoves = generate_good_moves(a, target)
             for move in nextMoves:
                 f, args = move
                 nextWord = f(*args)
                 if nextWord not in seenWords:
                     if nextWord == target:
-                        last = (target, move)
-                        break
+                        last = (target, prevMoves+[move])
                     else:
-                        queue.append((nextWord, move))
-    assert last is not None
-    moveList = []
-    while last != (word, None):
-        move, word ## oof can't backtrack if you clear the queue
+                        queue.append((nextWord, prevMoves+[move]))
+    return last[1]
+print(min_path('michael','chimelb'))
+### left off realizing that you should only replace "bad" characters using set arithmetic
+## queue explosion due to to many options (bfs too wide) is main problem
+# maybe go letter by letter and remove common letters from each word, and replace the letters that are left
+# maybe give remove, add, and replace less options to avoid queue explosion. Maybe only let them happen at
+# the beginning of the string, and let swap handle the rest?
