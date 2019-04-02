b1bd2c784052620327d5868def57e27e786e293b
Mike Delmonaco <mthundershock@gmail.com>
2019-04-02 11:44:57 -0400

    started bfs

--- python/stringMorph.py
+++ python/stringMorph.py
@@ -44,11 +44,80 @@ def generate_possible_moves(s):
 def generate_possible_next_states(s):
     moves = generate_possible_moves(s)
     next_states = set([])
+    next_states_with_moves = []
     for move in moves:
         f, args = move
-        next_states.add(f(*args))
-    return next_states
+        next_state = f(*args)
+        if next_state not in next_states:
+            next_states.add(next_state)
+            next_states_with_moves.add((next_state, move))
+    return next_states_with_moves
 
+"""
+def min_path(word, target):
+    '''
+    return minimum length list of moves
+    '''
+    # start string -> best path
+    # path stored as a list of moves
+    mem = {}
+    def help(a, maxlen):
+        '''
+        computes the best path from a to target
+        may return none if maxlen is too short
+        returns [] if it reaches the target
+        '''
+        if a in mem:
+            return mem[a]
+        if a == target:
+            return []
+        if maxlen == 0:
+            return None
+        nextMoves = generate_possible_moves(a)
+        bestPath = None
+        bestPathLength = float('inf')
+        for move in nextMoves:
+            f, args = move
+            nextWord = f(*args)
+            # best path from nextWord to target
+            bestRestPath = help(nextWord, maxlen-1)
+            if bestRestPath is not None:
+                path = [move] + bestRestPath
+                if len(path) < bestPathLength:
+                    bestPath = path
+                    bestPathLength = len(path)
+        mem[a] = bestPath
+        return bestPath
+    # don't let any sequence of transformations get longer than removing
+    # everything from a and adding b to it
+    return help(word, len(word) + len(target))
+"""
+# breadth first
+def min_path(word, target):
+    if word == target:
+        return []
+    # list of (word, move) pairs
+    queue = [(word, None)]
+    seenWords = set([])
+    last = None
+    while len(queue) > 0:
+        curr = queue.popLeft()
 
-def min_dist(a, b):
-    pass
+        if curr not in seenWords:
+            seenWords.add(curr)
+            a, prevMove = curr
+            prevWord = prevMove[0]
+            nextMoves = generate_possible_moves(a)
+            for move in nextMoves:
+                f, args = move
+                nextWord = f(*args)
+                if nextWord not in seenWords:
+                    if nextWord == target:
+                        last = (target, move)
+                        break
+                    else:
+                        queue.append((nextWord, move))
+    assert last is not None
+    moveList = []
+    while last != (word, None):
+        move, word ## oof can't backtrack if you clear the queue
