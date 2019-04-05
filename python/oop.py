083e9bbd546f61832ccc3eeb03f512007d5ce66c
Mike Delmonaco <mthundershock@gmail.com>
2019-04-05 10:22:03 -0400

    contructor, but fix

--- python/oop.py
+++ python/oop.py
@@ -11,22 +11,34 @@ tools: functions, lambdas, dictionaries
 '''
 
 def dot(obj, attribute_name, *args):
-    if attribute_name in obj[fields]:
+    if attribute_name in obj['fields']:
         return obj['fields'][attribute_name]
-    elif attribute_name in obj[methods]:
+    elif attribute_name in obj['methods']:
         return obj['methods'][attribute_name](*args)
     else:
         raise AttributeError('unknown attribute error: {}'.format(attribute_name))
 
-point = {
-    'fields': {
-        "x":3,
-        "y":4
-    },
-    'methods': {
+# point = {
+#     'fields': {
+#         "x":3,
+#         "y":4
+#     },
+#     'methods': {
 
+#     }
+# }
+def make_point(x, y):
+    point = {
+        'fields': {
+            'x':x,
+            'y':y
+        },
+        'methods': {
+
+        }
     }
-}
+    return point
 
 if __name__ == '__main__':
+    point = make_point(3, 5)
     print(dot(point, 'x'))
