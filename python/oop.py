52f8726fc2701e4e01caee1b43df008a04271a84
Mike Delmonaco <mthundershock@gmail.com>
2019-04-05 10:18:57 -0400

    field access

---
+++ python/oop.py
@@ -0,0 +1,32 @@
+'''
+an object is what an object has
+hashmap of fields and methods
+
+goals:
+inheritance
+dynamic dispatch
+field and method access
+
+tools: functions, lambdas, dictionaries
+'''
+
+def dot(obj, attribute_name, *args):
+    if attribute_name in obj[fields]:
+        return obj['fields'][attribute_name]
+    elif attribute_name in obj[methods]:
+        return obj['methods'][attribute_name](*args)
+    else:
+        raise AttributeError('unknown attribute error: {}'.format(attribute_name))
+
+point = {
+    'fields': {
+        "x":3,
+        "y":4
+    },
+    'methods': {
+
+    }
+}
+
+if __name__ == '__main__':
+    print(dot(point, 'x'))
