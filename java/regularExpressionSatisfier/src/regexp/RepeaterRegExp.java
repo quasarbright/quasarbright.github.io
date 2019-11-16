732cdf1bdaaaf395cbcc0c5b05255df42d638b74
quasarbright <mthundershock@gmail.com>
2019-11-16 02:03:01 -0500

    satisfiers

--- java/regularExpressionSatisfier/src/regexp/RepeaterRegExp.java
+++ java/regularExpressionSatisfier/src/regexp/RepeaterRegExp.java
@@ -4,4 +4,14 @@ package regexp;
  * Zero or more repetitions of a given regular expression.
  */
 public class RepeaterRegExp implements RegExp {
+  private final RegExp regExp;
+
+  public RepeaterRegExp(RegExp regExp) {
+    this.regExp = regExp;
+  }
+
+  @Override
+  public <R> R accept(RegexpVisitor<R> visitor) {
+    return visitor.visitRepeaterRegExp(regExp);
+  }
 }
