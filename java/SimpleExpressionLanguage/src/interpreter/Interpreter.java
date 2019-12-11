936d3fb1c711a38f6aa0064df7eb83fa7bd83971
Mike Delmonaco <mthundershock@gmail.com>
2019-12-10 19:49:48 -0500

    repl and cleaning

--- java/SimpleExpressionLanguage/src/interpreter/Interpreter.java
+++ java/SimpleExpressionLanguage/src/interpreter/Interpreter.java
@@ -10,16 +10,27 @@ import org.antlr.v4.runtime.TokenStream;
 import java.util.Scanner;
 
 public class Interpreter {
+    public static Double eval(String source) {
+        CharStream stream = CharStreams.fromString(source);
+        ExpGrammarLexer lexer = new ExpGrammarLexer(stream);
+        TokenStream tokenStream = new CommonTokenStream(lexer);
+        ExpGrammarParser parser = new ExpGrammarParser(tokenStream);
+        ExpGrammarParser.ExprContext parseTree = parser.expr();
+        return (new ExpEvaluator().visit(parseTree));
+    }
     public static void main(String[] args) {
+        System.out.println("prefix calculator REPL");
+        System.out.println("(+ 1 2 3 (* -3.5 4))");
+        System.out.println("-8.0");
         Scanner in = new Scanner(System.in);
         while(in.hasNextLine()) {
+            // read
             String source = in.nextLine();
-            CharStream stream = CharStreams.fromString(source);
-            ExpGrammarLexer lexer = new ExpGrammarLexer(stream);
-            TokenStream tokenStream = new CommonTokenStream(lexer);
-            ExpGrammarParser parser = new ExpGrammarParser(tokenStream);
-            ExpGrammarParser.ExprContext parseTree = parser.expr();
-            System.out.println(new ExpEvaluator().visit(parseTree));
+            // eval
+            Double val = eval(source);
+            // print
+            System.out.println(val);
+            // loop
         }
     }
 }
