dd3b2d85a2a8bfa6099134f6d48a6e573e8a8ddf
quasarbright <mthundershock@gmail.com>
2021-01-22 02:24:48 -0500

    documentation, extraction, now it's like a lib

--- p5js/3PointPerspective/point.js
+++ p5js/3PointPerspective/point.js
@@ -1,18 +1,64 @@
+/*
+
+constraint-based visualizations
+kind of reactive, but uses thunks basically
+
+draggable points have a special hook in the draw loop
+mostly, other logic is confined to updates
+
+there are draggable points, derived points,
+line segments, and sliders.
+
+draggable points can be dragged by the user and are unconstrained.
+Their position is updated when dragged.
+
+derived points have a position calculated by a thunk.
+It stores the thunk and caches its result, so
+you have to keep it updated manually.
+
+Segments take in two point-like objects and uses their cached positions
+every time they are drawn. (uses the .p field of a point)
+
+each class has a list of all instances, and the constructors add
+this to the list. these lists are used for world updates
+
+
+
+*/
+
+// INVARIANT: At most one point is selected
 let points = []
 selected = undefined
 
+/**
+ * select a draggable point and deselect all others
+ * @param {Point} p point to select
+ */
 function selectPoint(p) {
     deselectAll()
     p.selected = true
     selected = p
 }
 
+/**
+ * deselect all points
+ */
 function deselectAll() {
     selected = undefined
     points.forEach(p => p.selected = false)
 }
 
+/**
+ * draggable points.
+ */
 class Point {
+    /**
+     * create a draggable point
+     * @param {p5.Vector} p position
+     * @param {boolean} draggable is this point draggable?
+     * @param {number} rad radius
+     * @param {p5.color} c fill color
+     */
     constructor(p,draggable,rad,c) {
         this.p = p || createVector(0,0)
         this.draggable = draggable || false
@@ -22,6 +68,12 @@ class Point {
         points.push(this)
     }
 
+    /**
+     * linear interpolate between this point and other. 
+     * @param {number} r interpolation rate (should be in [0,1])
+     * @param {Point} other end point 
+     * @returns {p5.Vector} interpolated position
+     */
     lerp(r,other) {
         return p5.Vector.lerp(this.p,other.p,r)
     }
@@ -45,8 +97,18 @@ class DerivedPoint {
         this.fp = fp
         this.p = fp()
         this.rad=rad || 7
-        this.c = c || color(200)
+        this.c = c || color(108)
         derivedPoints.push(this)
+    }
+
+    /**
+     * linear interpolate between this point and other. 
+     * @param {number} r interpolation rate (should be in [0,1])
+     * @param {Point} other end point 
+     * @returns {p5.Vector} interpolated position
+     */
+    lerp(r, other) {
+        return p5.Vector.lerp(this.p, other.p, r)
     }
 
     update() {
@@ -61,12 +123,30 @@ class DerivedPoint {
     }
 }
 
+/**
+ * create a point that is interpolated between two points.
+ * @param {Point} start start point
+ * @param {Point} end end point
+ * @param {() => number} kf thunk to compute interpolation rate
+ * @returns {DerivedPoint}
+ */
 function lerpDerivedPoint(start,end,kf) {
     return new DerivedPoint(()=>start.lerp(kf(),end).copy())
 }
 
+
 let segments = []
+/**
+ * line segment between two points
+ */
 class Segment {
+    /**
+     * create a line segment between two points
+     * @param {Point} pi start point
+     * @param {Point} pf end point
+     * @param {number} thickness line thickness
+     * @param {pt.color} c stroke color
+     */
     constructor(pi,pf,thickness,c) { // points!
         this.pi = pi
         this.pf = pf
@@ -75,8 +155,8 @@ class Segment {
         segments.push(this)
     }
 
-    get startPos() {return pi.p}
-    get endPos() {return pf.p}
+    // get startPos() {return pi.p}
+    // get endPos() {return pf.p}
 
     draw() {
         stroke(this.c)
@@ -86,7 +166,18 @@ class Segment {
 }
 
 let slidables = []
+/**
+ * a float variable editable by the user
+ */
 class Slidable {
+    /**
+     * create a float float variable editable by a slider
+     * @param {string} name name of the variable
+     * @param {number} x value
+     * @param {number} min min value
+     * @param {number} max max value
+     * @param {number} step step size between values
+     */
     constructor(name,x,min,max,step) {
         this.name=name
         this.x = x
@@ -101,12 +192,23 @@ class Slidable {
     }
 }
 
+/**
+ * 
+ * @param {p5.Vector} v1 
+ * @param {p5.Vector} v2 
+ * @returns {number} squared distance between vectors v1,v2
+ */
 function distSq(v1,v2) {
-    
     let ans = p5.Vector.sub(v1,v2).magSq()
     return ans
 }
 
+/**
+ * 
+ * @param {p5.Vector} v 
+ * @param {Array.<Point>} points 
+ * @returns {?Point} the point which is closest to v, or null
+ */
 function closestPoint(v,points) {
     if (points.length > 0) {
         let best = points[0]
@@ -120,6 +222,55 @@ function closestPoint(v,points) {
         });
         return best
     } else {
-        return undefined
+        return null
     }
+}
+
+function initSliders() {
+    slidables.forEach(s => {
+        $("#inputs").append($(`<label for="${s.name}">${s.name}</label>`))
+        $("#inputs").append($(`<input class="slider" name="${s.name}" id="${s.name}" type="range" min="${s.min}" max="${s.max}" step="${s.step}" value="${s.x}">`))
+        $("#inputs").append($(`<hr>`))
+    })
+}
+
+function initWorld() {
+    initSliders()
+}
+
+//updaters
+function updateSelected() {
+    if (mouseIsPressed) {
+        mouse = createVector(mouseX, mouseY)
+        if (selected !== undefined) {
+            selected.p = mouse.copy()
+        } else {
+            draggables = points.filter(p => p.draggable && distSq(mouse, p.p) < p.rad * p.rad)
+            best = closestPoint(mouse, draggables)
+            if (best !== null) {
+                selectPoint(best)
+                best.p = mouse.copy()
+            }
+        }
+    } else {
+        deselectAll()
+    }
+}
+
+function updateWorld() {
+    //update sliders
+    slidables.forEach(s => s.update())
+    updateSelected()
+    //update derived point locations
+    derivedPoints.forEach(p => p.update())
+}
+
+function drawWorld() {
+    // draw segments
+    segments.forEach(s => s.draw())
+    // draw points
+    derivedPoints.forEach(p => p.draw())
+    points.forEach(p => { p.draw() })
+    // ensure selected points drawn on top
+    points.filter(p => p.selected).forEach(p => p.draw())
 }
--- p5js/3PointPerspective/sketch.js
+++ p5js/3PointPerspective/sketch.js
@@ -8,10 +8,10 @@ let bvert, tlvert, trvert // derived vertices of the box
 function setup() {
   createCanvas(600, 600);
   ellipseMode(RADIUS)
-  c = new Point(createVector(200, 200), true)
-  tl = new Point(createVector(50, 50), true)
-  tr = new Point(createVector(350, 50), true)
-  b = new Point(createVector(200, 350), true)
+  c = new Point(createVector(width/2, height*.2), true)
+  tl = new Point(createVector(width*0.05, height*0.05), true)
+  tr = new Point(createVector(width*0.95, height*0.05), true)
+  b = new Point(createVector(width/2, height*0.95), true)
   ctl = new Segment(c,tl)
   ctr = new Segment(c,tr)
   cb = new Segment(c,b)
@@ -35,42 +35,11 @@ function setup() {
   // new Segment(c,trvert,5,blue)
   // new Segment(c,bvert,5,blue)
 
-  // add slidables to doc
-  slidables.forEach(s=>{
-    $("#inputs").append($(`<label for="${s.name}">${s.name}</label>`))
-    $("#inputs").append($(`<input class="slider" name="${s.name}" id="${s.name}" type="range" min="${s.min}" max="${s.max}" step="${s.step}" value="${s.x}">`))
-    $("#inputs").append($(`<hr>`))
-  })
+  initWorld()
 }
 
 function draw() {
-  //update sliders
-  slidables.forEach(s=>s.update())
   background(51);
-  if(mouseIsPressed) {
-      mouse = createVector(mouseX,mouseY)
-      if(selected !== undefined) {
-        selected.p = mouse.copy()
-      } else {
-        draggables = points.filter(p => p.draggable && distSq(mouse,p.p) < p.rad * p.rad)
-        best = closestPoint(mouse,draggables)
-        if(best !== undefined) {
-          selectPoint(best)
-          best.p = mouse.copy()
-        }
-      }
-  } else {
-    deselectAll()
-  }
-
-  //update derived point locations
-  derivedPoints.forEach(p=>p.update())
-
-  // draw segments
-  segments.forEach(s=>s.draw())
-  // draw points
-  derivedPoints.forEach(p=>p.draw())
-  points.forEach(p=>{p.draw()})
-  // ensure selected points drawn on top
-  points.filter(p=>p.selected).forEach(p=>p.draw())
+  updateWorld()
+  drawWorld()
 }
